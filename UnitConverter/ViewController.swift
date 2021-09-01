2d30d26a0ccd5911c15a204696f935d20fdb35a1
Jarret Whitehouse <jarrwhit.iu.edu>
2021-09-01 17:26:09 -0400

    End of V1 - converting Miles to KM

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -8,6 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets and Actions
+    
+    @IBOutlet var convertedAmountLabel : UILabel!
+    @IBOutlet var amountToConvertText: UITextField!
+    
+    @IBAction func milesToKM(_ sender:UIButton) {
+        //convertedAmountLabel.text = "Yo"
+        if let miles = Double(amountToConvertText.text!) {
+            let km = milesToKM(miles)
+            convertedAmountLabel.text = "\(String(km)) kilometers"
+        }else {
+            convertedAmountLabel.text = "error"
+        }
+    }
+    
+    // MARK: Custom Functions
+    func milesToKM(_ miles: Double) -> Double {
+        return miles * 1.60934
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
